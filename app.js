6dbccbb73b05deb41d1f390c57d26a6edc00d5bc
rajanmah <146156379+rajanmah@users.noreply.github.com>
2023-10-21 15:58:14 -0400

    Wild Wild Life answered

--- app.js
+++ app.js
@@ -12,13 +12,14 @@
 // let evenNums = 0;
 for (let evenNums=0; evenNums<=200; evenNums+=2){
 
-    console.log(evenNums);
+    // console.log(evenNums);
 }
 
 
 
 //Fizz Buzz
 // let num =1;
+/*
 for (let num=0; num<=100; num++ ){
     if (num%3===0 && num%5===0){
         console.log("FizzBuzz")
@@ -30,3 +31,29 @@ for (let num=0; num<=100; num++ ){
             console.log(num)
         }
 }
+*/
+
+
+// Wild Wild Life
+const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+const sharky = ["Sharky", "shark", 20, "Left Coast"]
+const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+//1. 
+plantee[2]=5001;
+console.log(plantee)
+
+//2. 
+wolfy[3] = "Gotham City";
+console.log(wolfy);
+
+//3.
+dart[3]= "Upside Down, Hawkins";
+console.log(dart);
+
+//4.
+wolfy[0]="Gameboy";
+console.log(wolfy);
+
+//Yell at the Ninja Turtles
