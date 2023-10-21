f49fed401dbc887a7d2d2fcf4ffc3ffcd0556e7e
rajanmah <146156379+rajanmah@users.noreply.github.com>
2023-10-21 15:22:49 -0400

    Fizz Buzz answered

--- app.js
+++ app.js
@@ -15,3 +15,18 @@ for (let evenNums=0; evenNums<=200; evenNums+=2){
     console.log(evenNums);
 }
 
+
+
+//Fizz Buzz
+// let num =1;
+for (let num=0; num<=100; num++ ){
+    if (num%3===0 && num%5===0){
+        console.log("FizzBuzz")
+    } else if (num%5===0){
+        console.log("Buzz") 
+    } else if ( num%3===0) {
+            console.log("Fizz")
+        } else {
+            console.log(num)
+        }
+}
